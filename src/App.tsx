6bb8ce25c601c08dc48669b65163125440d79f64
Antonina Semkina <antonina.a.semkina@gmail.com>
2025-02-05 19:29:43 +0300

    Обновление проекта

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
-    <BrowserRouter basename="/MST">
+    <BrowserRouter basename="/MST/">
       <AppRouter />
     </BrowserRouter>
   );
